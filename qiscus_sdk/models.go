00d1522bbe0c64b3eb916097b2fd9db3186f7283
Kukuh Alfian <hi@kalfian.com>
2021-06-18 23:02:56 +0700

    [ADD] SDK Generator

---
+++ qiscus_sdk/models.go
@@ -0,0 +1,31 @@
+package qiscussdk
+
+var (
+	POST_COMMENT = "/api/v2.1/rest/post_comment"
+)
+
+type RequestWebhook struct {
+}
+
+type ParamQiscusSDK struct {
+	AppID    string
+	SecretID string
+	BaseUrl  string
+}
+
+type ParamSendMessage struct {
+	UserID  string `form:"user_id" json:"user_id"`
+	RoomID  string `form:"room_id" json:"room_id"`
+	Message string `form:"message" json:"message"`
+}
+
+type ResponseSendMessage struct {
+	Results struct {
+		Comment struct {
+			ID      int    `json:"id"`
+			Message string `json:"message"`
+		} `json:"comment"`
+	} `json:"results"`
+
+	Status int `json:"status"`
+}
---
+++ qiscus_sdk/sdk.go
@@ -0,0 +1,70 @@
+package qiscussdk
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type QiscusSdkContract interface {
+	SendMessage(request ParamSendMessage) (*ResponseSendMessage, error)
+
+	// Hit API SDK
+	HitAPI(payload interface{}, url string, method string) ([]byte, error)
+}
+
+type qiscusSdk struct {
+	AppID    string
+	SecretID string
+	BaseUrl  string
+}
+
+func NewQiscusSDK(request ParamQiscusSDK) QiscusSdkContract {
+	return &qiscusSdk{
+		AppID:    request.AppID,
+		SecretID: request.SecretID,
+		BaseUrl:  request.BaseUrl,
+	}
+}
+
+func (sdk *qiscusSdk) SendMessage(request ParamSendMessage) (*ResponseSendMessage, error) {
+	var response ResponseSendMessage
+
+	data, err := sdk.HitAPI(request, POST_COMMENT, "POST")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(data, &response); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func (sdk *qiscusSdk) HitAPI(payload interface{}, url string, method string) ([]byte, error) {
+	jsonPayload, _ := json.Marshal(&payload)
+
+	reqhttp, err := http.NewRequest(method, sdk.BaseUrl+url, bytes.NewReader(jsonPayload))
+	if err != nil {
+		return nil, err
+	}
+	reqhttp.Header.Add("Content-Type", "application/json")
+	reqhttp.Header.Add("QISCUS-SDK-APP-ID", sdk.AppID)
+	reqhttp.Header.Add("QISCUS-SDK-SECRET", sdk.SecretID)
+
+	res, err := http.DefaultClient.Do(reqhttp)
+
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
